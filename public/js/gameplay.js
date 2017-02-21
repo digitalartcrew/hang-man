3d097ee22057ff1913391a187903f3dd06a3d7a0
digitalartcrew <digitalartcrew@gmail.com>
2017-02-20 21:58:29 -1000

    Update

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -3,8 +3,7 @@ $('document').ready(function(){
         //start a new game
         $('#newgame').click(function(){
             localStorage.clear();
-            startGame();
-            requestWord();
+            requestSecret();
         });
 
 
@@ -14,16 +13,13 @@ $('document').ready(function(){
             var count = 0;
             //Record the guessed answer
             var guess = this.innerHTML.toLowerCase();
-            //Access the word to check if letters exists
-            var word = localStorage.answer;
+            //Access the secret to check if letters exists
+            var secret = localStorage.answer;
             //Access the index of correct guess
-            var i = word.indexOf(guess);
+            var i = secret.indexOf(guess);
             //check to see if the letter exists
-            if(word.indexOf(guess) > -1){
-                temp = localStorage.guessedLetters.split(',');
-                temp[i] = guess;
-                localStorage.guessedLetters = temp.join('');
-                 // 
+            if(secret.indexOf(guess) > -1){
+                console.log('Yes');
             } else {
                 console.log('No');
                 count++
@@ -36,35 +32,35 @@ $('document').ready(function(){
       
 
 
-        function wordify(list){
-                //Create an array from list of words
-                words = list.split('\n');
+        function secretify(list){
+                //Create an array from list of secrets
+                secrets = list.split('\n');
                 //Access the length of list to randomly select an index
-                listSize = words.length;
+                listSize = secrets.length;
                 //Randomly select an index
                 randomNum = Math.floor((Math.random() * listSize) + 0);
-                //Accress random word
-                newWord = words[randomNum];
+                //Accress random secret
+                newScret = secrets[randomNum];
                 //creates space that can be updated in local storage
-                showLetters = newWord.split('').map(function(x){
+                showLetters = newScret.split('').map(function(x){
                     return '-'
                 });
                 //display space on the page
                 document.getElementById('guessbox').innerHTML = showLetters.join('');
-                //Store new word in local storage to check  
-                localStorage.setItem("answer",newWord);
+                //Store new secret in local storage to check  
+                localStorage.setItem("answer",newScret);
                 //Store spaces to be updated
                 localStorage.setItem("guessedLetters",showLetters);
         }
 
-	    function requestWord() {
+	    function requestSecret() {
         $.ajax({
             type: "GET",
             datatype: 'jsonp', 
             url:"https://linkedin-words.herokuapp.com/words", 
             success: function(res){
              localStorage.clear();
-             wordify(res);
+             secretify(res);
         }, 
             failure: function(err){
                 console.log(err);
