5ba94f89c3e0f3d6d5efb4a547c66be537ba6eed
digitalartcrew <digitalartcrew@gmail.com>
2017-02-20 23:22:53 -1000

    displaying head

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -1,5 +1,75 @@
 $('document').ready(function(){
 
+    //Hangman begins
+    var x = 400;
+    var y = 120;
+    //head
+    var canvas = document.getElementById('hangman');
+    var ctx = canvas.getContext('2d');
+
+    function displayhead(){
+        ctx.beginPath();
+        ctx.arc(x, y, 50, 0, Math.PI * 2, true); // Outer circle
+        ctx.stroke();
+    }
+
+    // body
+    function displaybody(){
+        ctx.beginPath();
+        ctx.moveTo(x, y + 50);
+        ctx.lineTo(x, y + 175);
+        ctx.stroke();
+    }
+
+    //left arm
+    function leftarm(){ 
+        ctx.beginPath();
+        ctx.moveTo(x, y + 75);
+        ctx.lineTo(x - 45, y + 85);
+        ctx.stroke();
+    }
+
+     //right arm
+     function rightarm(){
+        ctx.beginPath();
+        ctx.moveTo(x, y + 75);
+        ctx.lineTo(x + 50, y + 85);
+        ctx.stroke(); 
+    }
+
+      //left leg
+      function leftleg(){
+        ctx.beginPath();
+        ctx.moveTo(x, y + 175);
+        ctx.lineTo(x - 40, y + 225);
+        ctx.stroke();
+    }
+
+     //right leg
+     function rightleg(){
+        ctx.beginPath();
+        ctx.moveTo(x, y + 175);
+        ctx.lineTo(x + 50, y + 225);
+        ctx.stroke();
+    } 
+
+    //stand
+    function stand(){
+        ctx.beginPath();
+        ctx.moveTo(x, y - 110);
+        ctx.lineTo(x, y - 50);
+        ctx.moveTo(x, y - 110);
+        ctx.lineTo(x - 200, y - 110);
+        ctx.moveTo(x-200, y - 110);
+        ctx.lineTo(x-200, y + 240);
+        ctx.moveTo(x-160, y + 240);
+        ctx.lineTo(x-240, y +240);
+        ctx.stroke();
+    }
+
+    stand();
+        //Hangman ends
+
         var guessCount = 6;
         var missedLetters = [];
 
@@ -14,30 +84,28 @@ $('document').ready(function(){
 
 
 
-       $('.letter').click(function(){
+        $('.letter').click(function(){
             var guess = this.innerHTML.toLowerCase();
             var secret = localStorage.answer;
             var i = secret.indexOf(guess);
-           
             //check to see if the letter exists
-
             if(secret.indexOf(guess) > -1){
-
                 console.log('Yes');
             } else {
                 missedLetters.push(guess);
                 document.getElementById('missed').innerHTML = missedLetters;
+                displayhead();
                 guessCount--;
                 if(guessCount === 0){
                     document.getElementById('results').innerHTML = "Sorry, you lose."
                 }
             }
-  
-        
-     
-       });
 
-      
+
+
+        });
+
+
 
 
         function secretify(list){
@@ -59,22 +127,22 @@ $('document').ready(function(){
                 localStorage.setItem("answer",newScret);
                 //Store spaces to be updated
                 localStorage.setItem("guessedLetters",showLetters);
-        }
-
-	    function requestSecret() {
-        $.ajax({
-            type: "GET",
-            datatype: 'jsonp', 
-            url:"https://linkedin-words.herokuapp.com/words", 
-            success: function(res){
-             localStorage.clear();
-             secretify(res);
-        }, 
-            failure: function(err){
-                console.log(err);
             }
-        });
-    }
+
+            function requestSecret() {
+                $.ajax({
+                    type: "GET",
+                    datatype: 'jsonp', 
+                    url:"https://linkedin-words.herokuapp.com/words", 
+                    success: function(res){
+                       localStorage.clear();
+                       secretify(res);
+                   }, 
+                   failure: function(err){
+                    console.log(err);
+                }
+            });
+            }
 
 
-})
+        })
