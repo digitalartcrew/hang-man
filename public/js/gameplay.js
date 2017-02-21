0f5ed07d8904d0e93f4d23dfab1fc01be600a89d
digitalartcrew <digitalartcrew@gmail.com>
2017-02-20 18:02:56 -1000

    Update

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -1,29 +1,82 @@
 $('document').ready(function(){
 
+        function Player(name,turn,score){
+            this.name = name;
+            this.score = score;
+            this.turn = false;
 
-        // function guessLetter(){
+        }
 
-        // }
+        var computer = new Player("Computer",false,0);
+        var user = new Player("User",false,0);
+
+        $('#newgame').click(function(){
+            localStorage.clear();
+            startGame();
+            requestWord();
+        });
+
+        function startGame(){
+            var flip = Math.random();
+
+            if(flip > 0.5){
+                computer.turn = true;
+                document.getElementById('user').innerHTML =" "
+                document.getElementById('computer').innerHTML =" Current User"
+            } else {
+                user.turn = true;
+                document.getElementById('computer').innerHTML =" "
+                document.getElementById('user').innerHTML =" Current User"
+            }
+        };
+
 
        $('.letter').click(function(){
-            console.log(this.innerHTML.toLowerCase());
-       })
+            //Use count to count the number of guesses
+            var count = 0;
+            //Record the guessed answer
+            var guess = this.innerHTML.toLowerCase();
+            //Access the word to check if letters exists
+            var word = localStorage.answer;
+            //Access the index of correct guess
+            var i = word.indexOf(guess);
+            //check to see if the letter exists
+            if(word.indexOf(guess) > -1){
+                temp = localStorage.guessedLetters.split(',');
+                temp[i] = guess;
+                localStorage.guessedLetters = temp.join('');
+                 // 
+            } else {
+                console.log('No');
+                count++
+            }
+  
+        
+     
+       });
+
+      
+
 
         function wordify(list){
-               words = list.split('\n');
+                //Create an array from list of words
+                words = list.split('\n');
+                //Access the length of list to randomly select an index
                 listSize = words.length;
+                //Randomly select an index
                 randomNum = Math.floor((Math.random() * listSize) + 0);
+                //Accress random word
                 newWord = words[randomNum];
-                splitWord = newWord.split('');
-                var hiddenWord = splitWord.map(function(x){
+                //creates space that can be updated in local storage
+                showLetters = newWord.split('').map(function(x){
                     return '-'
-                })
-
-                document.getElementById('guessbox').innerHTML = hiddenWord.join('');
-                console.log(newWord + '\n' + splitWord + '\n' + hiddenWord);
-                
+                });
+                //display space on the page
+                document.getElementById('guessbox').innerHTML = showLetters.join('');
+                //Store new word in local storage to check  
                 localStorage.setItem("answer",newWord);
-                console.log(localStorage);
+                //Store spaces to be updated
+                localStorage.setItem("guessedLetters",showLetters);
         }
 
 	    function requestWord() {
@@ -41,8 +94,5 @@ $('document').ready(function(){
         });
     }
 
-
-
-    requestWord();
 
 })
